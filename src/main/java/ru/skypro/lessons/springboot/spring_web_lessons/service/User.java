35bd3f61d3107565f1fdfd2896536677065f8d7c
AynurZaydullin <zaydullin.aynur@yandex.ru>
2023-07-12 17:27:07 +0300

    Начало выполнения ДЗ по теме Spring Security.

---
+++ src/main/java/ru/skypro/lessons/springboot/spring_web_lessons/service/User.java
@@ -0,0 +1,47 @@
+package ru.skypro.lessons.springboot.spring_web_lessons.service;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String password;
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
