2b9979b2e1986164016744091b788dcb184b3dd8
AynurZaydullin <zaydullin.aynur@yandex.ru>
2023-07-20 15:38:47 +0500

    Initial commit.

--- src/main/java/ru/skypro/lessons/springboot/spring_web_lessons/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.skypro.lessons.springboot.spring_web_lessons.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.lessons.springboot.spring_web_lessons.service.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/ru/skypro/lessons/springboot/spring_web_lessons/service/Role.java
+++
@@ -1,6 +0,0 @@
-package ru.skypro.lessons.springboot.spring_web_lessons.service;
-
-public enum Role {
-    USER,
-    ADMIN
-}
